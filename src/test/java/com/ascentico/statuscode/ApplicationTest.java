c5c72dd516f145a33b00bd8300e34d5fb92d4f2e
Andy McCall <mailme@andymccall.co.uk>
2018-07-24 11:12:23 +0100

    Initial commit

---
+++ src/test/java/com/ascentico/statuscode/ApplicationTest.java
@@ -0,0 +1,11 @@
+package com.ascentico.statuscode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ApplicationTest {
+    @Test
+    public void main() throws Exception {
+        Assert.assertTrue(true);
+    }
+}
